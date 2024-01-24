6f2f5521e0d29466a885ecae4a0366fbba724f04
Johan Chataigner <jchataigner@betomorrow.com>
2024-01-24 17:00:22 +0100

    Use managed identity to pull images from acr

--- src/app-service.tf
+++ src/app-service.tf
@@ -3,7 +3,7 @@ resource "azurerm_service_plan" "games_backend" {
   name                = "gamesserverbackend"
   resource_group_name = data.azurerm_resource_group.test_terraform.name
   os_type             = "Linux"
-  sku_name            = "F1"
+  sku_name            = "B1"
 }
 
 resource "azurerm_linux_web_app" "games_backend" {
@@ -17,12 +17,13 @@ resource "azurerm_linux_web_app" "games_backend" {
   }
 
   site_config {
+    container_registry_use_managed_identity = true
     application_stack {
       docker_registry_url = "https://${azurerm_container_registry.backend_registry.login_server}"
       docker_image_name   = "games-server-backend"
     }
 
     health_check_path = "/actuator/health"
-    always_on = false
+    always_on = true
   }
 }
